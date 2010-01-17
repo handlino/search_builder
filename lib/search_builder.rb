f77dbba6d5ca6196339115f86e7e73424211ed24
Wei Jen Lu <weijenlu@gmail.com>
2010-01-17 14:34:34 +0800

    search conditions will consider the adapter

--- lib/search_builder.rb
+++ lib/search_builder.rb
@@ -3,20 +3,26 @@ module SearchBuilder
     base.extend(ClassMethods)
   end
 
-  #def conditions
-  #  [conditions_clauses.join(' AND '), *conditions_options]
-  #end
-
-  #def conditions_clauses
-  #  conditions_parts.map { |condition| condition.first }
-  #end
-
-  #def conditions_options
-  #  conditions_parts.map { |condition| condition[1..-1] }.flatten
-  #end
-
   def conditions
     methods.grep(/_searchable_conditions$/).map { |m| self.class.merge_conditions(send(m)) }.compact.join(" AND ")
+  end
+
+  def table_name_for_adapter
+    case self.class.connection.class.to_s
+    when /SQLServer/
+      "[#{self.class.table_name}]"
+    else
+      self.class.table_name
+    end
+  end
+
+  def column_name_for_adapter(column)
+    case self.class.connection.class.to_s
+    when /SQLServer/
+      "[#{column}]"
+    else
+      column
+    end
   end
 
 
@@ -25,7 +31,7 @@ module SearchBuilder
       self.class_eval do
         columns.each do |column|
           define_method("#{column}_like_searchable_conditions") do
-            ["#{self.class.table_name}.#{column} like ?", "%#{self.send(column)}%"] unless self.send(column).blank?
+            ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} like ?", "%#{self.send(column)}%"] unless self.send(column).blank?
           end
         end
       end
@@ -35,7 +41,7 @@ module SearchBuilder
       self.class_eval do
         columns.each do |column|
           define_method("#{column}_equal_searchable_conditions") do
-            ["#{self.class.table_name}.#{column} = ?", self.send(column)] unless self.send(column).blank?
+            ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} = ?", self.send(column)] unless self.send(column).blank?
           end
         end
       end
@@ -45,7 +51,7 @@ module SearchBuilder
       self.class_eval do
         columns.each do |column|
           define_method("#{column}_less_than_searchable_conditions") do
-            ["#{self.class.table_name}.#{column} < ?", self.send(column)] unless self.send(column).blank?
+            ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} < ?", self.send(column)] unless self.send(column).blank?
           end
         end
       end
@@ -55,7 +61,7 @@ module SearchBuilder
       self.class_eval do
         columns.each do |column|
           define_method("#{column}_less_and_equal_than_searchable_conditions") do
-            ["#{self.class.table_name}.#{column} <= ?", self.send(column)] unless self.send(column).blank?
+            ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} <= ?", self.send(column)] unless self.send(column).blank?
           end
         end
       end
@@ -65,7 +71,7 @@ module SearchBuilder
       self.class_eval do
         columns.each do |column|
           define_method("#{column}_greater_than_searchable_conditions") do
-            ["#{self.class.table_name}.#{column} > ?", self.send(column)] unless self.send(column).blank?
+            ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} > ?", self.send(column)] unless self.send(column).blank?
           end
         end
       end
@@ -75,7 +81,7 @@ module SearchBuilder
       self.class_eval do
         columns.each do |column|
           define_method("#{column}_greater_and_equal_than_searchable_conditions") do
-            ["#{self.class.table_name}.#{column} >= ?", self.send(column)] unless self.send(column).blank?
+            ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} >= ?", self.send(column)] unless self.send(column).blank?
           end
         end
       end
@@ -88,7 +94,7 @@ module SearchBuilder
         attr_accessor end_column
 
         define_method("#{column}_between_searchable_conditions") do
-          ["#{self.class.table_name}.#{column} BETWEEN ? AND ?", Date.parse(self.send(begin_column)).at_beginning_of_day, Date.parse(self.send(end_column)).end_of_day] unless self.send(begin_column).blank? || self.send(end_column).blank?
+          ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} BETWEEN ? AND ?", Date.parse(self.send(begin_column)).at_beginning_of_day, Date.parse(self.send(end_column)).end_of_day] unless self.send(begin_column).blank? || self.send(end_column).blank?
         end
       end
     end
@@ -100,7 +106,7 @@ module SearchBuilder
         attr_accessor end_column
 
         define_method("#{column}_between_searchable_conditions") do
-          ["#{self.class.table_name}.#{column} BETWEEN ? AND ?", self.send(begin_column).to_i, self.send(end_column).to_i] unless self.send(begin_column).blank? || self.send(end_column).blank?
+          ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} BETWEEN ? AND ?", self.send(begin_column).to_i, self.send(end_column).to_i] unless self.send(begin_column).blank? || self.send(end_column).blank?
         end
       end
     end
@@ -112,7 +118,7 @@ module SearchBuilder
         attr_accessor end_column
 
         define_method("#{column}_between_searchable_conditions") do
-          ["#{self.class.table_name}.#{column} BETWEEN ? AND ?", self.send(begin_column).to_f, self.send(end_column).to_f] unless self.send(begin_column).blank? || self.send(end_column).blank?
+          ["#{table_name_for_adapter}.#{column_name_for_adapter(column)} BETWEEN ? AND ?", self.send(begin_column).to_f, self.send(end_column).to_f] unless self.send(begin_column).blank? || self.send(end_column).blank?
         end
       end
     end
